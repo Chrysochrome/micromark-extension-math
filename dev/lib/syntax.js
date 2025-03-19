1c3af2bba119f2ae7829d0e0c2d2c44e32b0d6fe
Chrysochrome <15140725+Chrysochrome@users.noreply.github.com>
2025-03-19 17:01:51 +0800

    feat: add math bracket flow and parens text constructs
    - Implemented `mathBracketFlow` for handling math expressions enclosed in brackets.
    - Added `mathParensText` for parsing math expressions within parentheses.
    - Updated `syntax.js` to include new constructs for flow and text parsing.
    - Created `math-bracket-flow.js` and `math-parens-text.js` files for the new functionalities.
    - Updated TypeScript build info to include new files.

--- dev/lib/syntax.js
+++ dev/lib/syntax.js
@@ -6,6 +6,8 @@
 import {codes} from 'micromark-util-symbol'
 import {mathFlow} from './math-flow.js'
 import {mathText} from './math-text.js'
+import {mathBracketFlow} from './math-bracket-flow.js'
+import {mathParensText} from './math-parens-text.js'
 
 /**
  * Create an extension for `micromark` to enable math syntax.
@@ -18,7 +20,13 @@ import {mathText} from './math-text.js'
  */
 export function math(options) {
   return {
-    flow: {[codes.dollarSign]: mathFlow},
-    text: {[codes.dollarSign]: mathText(options)}
+    flow: {
+      [codes.dollarSign]: mathFlow,
+      [codes.backslash]: mathBracketFlow
+    },
+    text: {
+      [codes.dollarSign]: mathText(options),
+      [codes.backslash]: mathParensText(options)
+    }
   }
 }
